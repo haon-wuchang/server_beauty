c64f369b74e0638b4950b9ca5b36a33fa6a32c53
haon <haoony@naver.com>
2025-04-23 14:27:18 +0900

    d

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -17,9 +17,9 @@ export const paymentKakaoPay = async(req, res) => {
                 quantity: 1,
                 total_amount,
                 tax_free_amount: 0,
-                approval_url: "http://localhost:3000/payment/success",
-                cancel_url: "http://localhost:3000/payment/cancel",
-                fail_url: "http://localhost:3000/payment/fail",
+                approval_url: "http://joseon-client.s3-website.ap-northeast-2.amazonaws.com/payment/success",
+                cancel_url: "http://joseon-client.s3-website.ap-northeast-2.amazonaws.com/payment/cancel",
+                fail_url: "http://joseon-client.s3-website.ap-northeast-2.amazonaws.com/payment/fail",
             },
             {
                 headers: {
